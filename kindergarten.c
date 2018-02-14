783a7f195ea8ed64e963a39b3102ea60fde82027
Beesham Sarendranauth <sarendr@cs.uwindsor.ca>
2018-02-14 16:00:43 -0500

    Add function prototype to color image

--- kindergarten.c
+++ kindergarten.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int colorImage(int color, int quadtrant, int fd); //Prototype
 
 int main(int argc, char *argv[]) {
 
@@ -8,6 +13,7 @@ int main(int argc, char *argv[]) {
     char fileErr[] = "Error creating file";
     char fileWriteErr[] = "Error writing to file";
     int fd;
+    int status;
 
     int red =  110;
     int green = 120;
@@ -22,6 +28,8 @@ int main(int argc, char *argv[]) {
     int width = 1000;
     int height = 1000;
 
+    int numOfProcesses = 2;
+
     char fileHeader[] = "P6\n1000 1000\n255\n";
 
     if(argc  != 7) {
@@ -29,17 +37,35 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
+    //Create the file
     if((fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0755)) == -1) write(STDOUT_FILENO, fileErr, sizeof(fileErr));
     else {
+        //Write header to file
         if((write(fd, fileHeader, sizeof(fileHeader))) == -1) {
             write(STDOUT_FILENO, fileWriteErr, sizeof(fileWriteErr));
             close(fd);
             exit(1);
         }
-            
-        
+       
+        int pid;
+        for(int i = 0; i < numOfProcesses; i++, pid = wait(&status)) {
+            if(fork() == 0) {
+                colorImage(green, 1, fd);
+            }
+        }
         close(fd);
     }
 
 
 }
+
+/*
+    Writes to a file the color. Paints a specific quadrant a specific color
+    params int color, int quadrant, file descriptor
+    returns 1 on success, 0 on failure 
+*/
+int colorImage(int color, int quadtrant, int fd) {
+
+    return 0;
+}
+
