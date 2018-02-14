eb5cfc1a120307b919a8d4b63640153a9449d7e8
Beesham Sarendranauth <sarendr@cs.uwindsor.ca>
2018-02-13 22:24:51 -0500

    Write header to file

--- kindergarten.c
+++ kindergarten.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
 
     char invalidArgCount[] = "Invalid amount of arguments";
     char fileErr[] = "Error creating file";
+    char fileWriteErr[] = "Error writing to file";
     int fd;
 
     int red =  110;
@@ -18,15 +19,25 @@ int main(int argc, char *argv[]) {
     int ocean = 180;
     int violet = 190;
 
-    if(argc  != 6) {
+    int width = 1000;
+    int height = 1000;
+
+    char fileHeader[] = "P6\n1000 1000\n255\n";
+
+    if(argc  != 7) {
         write(STDOUT_FILENO, invalidArgCount, sizeof(invalidArgCount));    
         exit(0);
     }
 
     if((fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0755)) == -1) write(STDOUT_FILENO, fileErr, sizeof(fileErr));
     else {
+        if((write(fd, fileHeader, sizeof(fileHeader))) == -1) {
+            write(STDOUT_FILENO, fileWriteErr, sizeof(fileWriteErr));
+            close(fd);
+            exit(1);
+        }
+            
         
-
         close(fd);
     }
 
